812b2603ed77d19ab3aacddace4ff3247fd04f5c
Leo Cheung <leo@mindcandy.com>
2013-06-11 23:55:04 +0100

    added restaurants and made restaurant view work

--- routes/rra.js
+++ routes/rra.js
@@ -1,38 +1,41 @@
 // our 'database'
 var restaurants = {
-    SKN:{name:'Shuriken', rating:10},
-    ASK:{name:'Ashiko', rating:6},
-    CGI:{name:'Chigiriki', rating:2},
-    NGT:{name:'Naginata', rating:9},
-    KTN:{name:'Katana', rating:10}
+    Harvester:{name:'Harvester', rating:5},
+    Hawksmoor:{name:'Hawksmoor', rating:10},
+    BurgerKing:{name:'Burger King', rating:2},
+    Benihana:{name:'Benihana', rating:6},
+    PizzaExpress:{name:'Pizza Express', rating:7}
 };
 
 exports.home = function (req, res) {
-	if (typeof req.session.username == 'undefined') res.render('home', { title:'restaurant rater app' });
-	else res.redirect('/restaurants')
+	if (typeof req.session.username == 'undefined') {
+		res.render('home', { title:'Rrestaurant Rater App' });
+	} else {
+		res.redirect('/restaurants')
+	}
 };
 
 // handler for form submitted from homepage
 exports.home_post_handler = function(req, res) {
-	username = req.body.username || 'Anonymous';
+	username = req.body.username || 'Noob';
 	req.session.username = username;
 	res.redirect('/');
 };
 
-//handler for displaying each restauarant
+//handler for displaying all restauarants
 exports.restaurants = function(req, res) {
 	if (typeof req.session.username == 'undefined') {
 		res.redirect('/');
 	} else {
-		res.render('restaurants', {title: 'Restaurants', username: req.session.username, restaurants:restaurants});
+		res.render('restaurants', {restaurants: restaurants});
 	}
 };
 
 //handler for displaying individual restaurants
 exports.restaurant = function(req, res) {
-	if (typeof req.session.username == 'undefined') {
-		res.redirect('/');
-	} else {
-		res.render('restaurant', {restaurants: restaurants});
-	}
+	var name = restaurants[req.params.id].name;
+    var rating = restaurants[req.params.id].rating;
+    res.render('restaurant', { name:name, rating:rating });
 };
+
+
